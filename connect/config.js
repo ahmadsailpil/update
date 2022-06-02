a623d2da0dabb838a5c23126cf3b5f779b52224d
dawawasgg <pnbsave@gmail.com>
2022-06-02 13:56:49 +0800

    der

---
+++ connect/config.js
@@ -0,0 +1,50 @@
+/**
+ - Create By ZackMans Official
+ - Contact Me on https://wa.me/+6281385062956
+ - Follow iG : @salman_alfarizi_15
+*/
+
+const fs = require("fs")
+const chalk = require("chalk")
+
+// Other
+global.thum = fs.readFileSync("./worker/media/image/thumb.jpg")
+global.log0 = fs.readFileSync("./worker/media/image/thumb.jpg")
+global.err4r = fs.readFileSync("./worker/media/image/thumb.jpg")
+global.owner = ['6283160327945']
+global.rkyt = []
+global.banUser = ['6281366679021@s.whatsapp.net']
+global.ntilink = ['120363043965529911@g.us','120363022988111568@g.us']
+global.wlcm = ['120363043965529911@g.us','120363023112413670@g.us','19054091487-1424650263@g.us','120363022988111568@g.us']
+global.gcrevoke = ['19054091487-1424650263@g.us']
+global.packname = "Buatan Siapa? Buatan Ridho\nMau nomor nya? 083160327945\nOrangnya ganteng dan baik\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+global.packname2 = "BY Ridho"
+global.author = "WM RIDHO"
+global.sessionName = "session"
+global.linkgrupss = "https://chat.whatsapp.com/DPGZ1QWn0zDGfxS8Xm7Kt9"
+global.antitags = true
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.mess = {
+    success: 'Success',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    linkm: 'Linknya Mana Kak?',
+    ban: 'Kamu telah dibanned oleh owner, Jika ingin di unbanned chat owner'
+}
+global.limitawal = {
+    rakyat: "Infinity",
+    free: 100
+}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
